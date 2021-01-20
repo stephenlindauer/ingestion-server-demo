a22a6ca4d850a1ac9baa74753211e6199c6648bb
Stephen Lindauer <slindauer@fb.com>
2021-01-20 14:26:29 -0800

    write ingested segments to files

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 const port = 3000;
+const fs = require("fs");
 
 var middleware = function (req, res, next) {
   console.log(req.buf);
@@ -19,12 +20,13 @@ var middleware = function (req, res, next) {
 app.use(cors({ origin: true, credentials: true }));
 // app.use(middleware);
 
-
-app.use(bodyParser.raw({
-  inflate: true,
-  limit: "50mb",
-  type: "application/octet-stream",
-}));
+app.use(
+  bodyParser.raw({
+    inflate: true,
+    limit: "50mb",
+    type: "application/octet-stream",
+  })
+);
 
 app.use(morgan("dev"));
 
@@ -34,11 +36,23 @@ app.get("/", (req, res) => {
 
 app.put("/upload", (req, res) => {
   try {
-    var contype = req.headers["content-type"];
-    console.log(contype);
-
-    // console.log(req);
     console.log(req.body);
+    const buffer = req.body;
+    const queryParams = req.query;
+
+    const segment_filename = `uploads/${queryParams.broadcastID}_${queryParams.filename}`;
+    const stream_filename = `uploads/${queryParams.broadcastID}.webm`;
+    fs.writeFile(segment_filename, buffer, function (err) {
+      if (err) return console.log(err);
+      console.log("Wrote file " + segment_filename);
+    });
+    if (queryParams.filename != "dash_manifest.mpd") {
+      // Manifest file doesn't need to be written to the stream file
+      fs.appendFile(stream_filename, buffer, function (err) {
+        if (err) return console.log(err);
+        console.log("Wrote file " + stream_filename);
+      });
+    }
 
     res.status(200).send({
       status: true,
@@ -49,33 +63,6 @@ app.put("/upload", (req, res) => {
     res.status(200).send({
       status: true,
       message: "error, but give 200 anyway",
-    });
-  }
-  return;
-
-  if (!req.files) {
-    console.log("[no files!");
-    res.status(400).send({
-      status: false,
-      message: "No file uploaded",
-    });
-  } else {
-    console.log(req.files);
-    //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-    let avatar = req.files.avatar;
-
-    //Use the mv() method to place the file in upload directory (i.e. "uploads")
-    avatar.mv("./uploads/" + avatar.name);
-
-    //send response
-    res.send({
-      status: true,
-      message: "File is uploaded",
-      data: {
-        name: avatar.name,
-        mimetype: avatar.mimetype,
-        size: avatar.size,
-      },
     });
   }
 });
